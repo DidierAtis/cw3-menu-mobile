2d73062b8c42eb3cbcc2db9bc44f091fe7fe4729
DidierAtis <70582678+DidierAtis@users.noreply.github.com>
2020-10-06 09:30:20 -0400

    Version Finale

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+
+    var hamburgers = document.querySelectorAll('.hamburger');
+    var menuMobile = document.querySelector('.nav-primary-mobile')
+
+    for(let i=0;i< hamburgers.length; i++){
+
+        var hamburger = hamburgers[i];
+        hamburger.addEventListener('click', openMenu);
+    }
+
+
+
+    function openMenu(evt){
+
+
+        evt.preventDefault();
+
+        var cible = evt.currentTarget;
+
+
+        if(cible.classList.contains("open")){
+            console.log("remove")
+            cible.classList.remove("open");
+            menuMobile.classList.remove("open");
+        }else {
+            console.log("add")
+
+            cible.classList.add("open");
+            menuMobile.classList.add("open");
+        }
+    }
+
 });
